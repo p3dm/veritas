eebaabc5497656d2654da98780d85897fb1a172b
p3dm <manh.pd225361@sis.hust.edu.vn>
2025-08-19 14:57:53 +0700

    frontend

---
+++ src/components/Hero.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Navbar from "@/components/ui/Navbar";
+
+const Hero = () => {
+  return (
+    <div>
+        <div>
+            <Navbar />
+        </div>
+        <div className="pt-30 pb-20 py-20 dark:bg-white bg-gray-100">
+
+        </div>
+    </div>
+  )
+}
+export default Hero
